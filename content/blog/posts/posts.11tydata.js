7a99e216e5fafcdf11b8ea84867ebac6cb762bdc
Dave Hulbert <dave1010@gmail.com>
2025-10-28 14:12:10 +0000

    Organize blog posts by date directories

--- content/blog/posts/posts.11tydata.js
+++ content/blog/posts/posts.11tydata.js
@@ -6,6 +6,20 @@ export default {
       const slug = (data.page.fileSlug || "")
         .replace(/^\d{4}-\d{2}-\d{2}-/, "")
         .replace(/\/+$/, "");
+      const date = data.page?.date instanceof Date
+        ? data.page.date
+        : data.page?.date
+          ? new Date(data.page.date)
+          : undefined;
+      const year = date?.getUTCFullYear();
+      const month = date
+        ? String(date.getUTCMonth() + 1).padStart(2, "0")
+        : undefined;
+
+      if (year && month) {
+        return `/blog/${year}/${month}/${slug || data.page.fileSlug}/`;
+      }
+
       return `/blog/${slug || data.page.fileSlug}/`;
     },
     isoDate: (data) => {
