e5fb8011efc99e0ad68d4e130320657a4bcdae0e
Dave Hulbert <dave1010@gmail.com>
2025-10-28 10:32:33 +0000

    Convert site to Eleventy project

---
+++ eleventy.config.js
@@ -0,0 +1,12 @@
+export default function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ public: "." });
+
+  return {
+    dir: {
+      input: "content",
+      layouts: "../src/layouts"
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+}
